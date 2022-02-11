9c42e05b5d347f00e18d648cb18a69fd4fe13f89
kgb <kgiandepot@gmail.com>
2022-02-11 19:24:05 +0200

    Create 16x16 grid

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 console.log('adf');
 
+const sketchContainer = document.querySelector('#sketchContainer');
+
+let gridSize = 16; // 16 x 16 grid;
+
+//i = row
+for (let i = 0; i < gridSize; i++) {
+    //j = column
+    const rowContainer = document.createElement('div');
+    rowContainer.className = 'rowContainer';
+    rowContainer.setAttribute('style','display:flex;');
+    
+    for (let j = 0; j < gridSize; j++) {
+        const div = document.createElement('div');
+        div.textContent = `(${j})`;
+        rowContainer.appendChild(div);
+        //create div for each j => 16 div
+    }
+    //create row
+    sketchContainer.appendChild(rowContainer);
+}
