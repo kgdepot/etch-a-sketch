b54d87f6194fe1a950f43a45c7394904e7e03c04
kgb <kgiandepot@gmail.com>
2022-02-12 16:33:29 +0200

    Change div color on mouse over

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 const sketchContainer = document.querySelector('#sketchContainer');
 
+
 let gridSize = 16; // 16 x 16 grid;
 
 
 for (let i = 0; i < gridSize; i++) {  //i = row
     const rowContainer = document.createElement('div');
     rowContainer.className = 'rowContainer';
-    rowContainer.setAttribute('style','display: flex;');
-    
+    rowContainer.setAttribute('style', 'display: flex;');
+
     for (let j = 0; j < gridSize; j++) {   //j = column
         const div = document.createElement('div');
         div.className = 'gridDiv';
-        div.textContent = `(${i},${j})`;
+        div.addEventListener('mouseover', setColor);
+            div.textContent = `${i},${j}`;
         div.style.backgroundColor = 'blue';
-        div.setAttribute('style', 'width: 50px; height: 50px;');
+
+        div.setAttribute('style', 'width: 40px; height: 40px;');
 
         rowContainer.appendChild(div);
         //create div for each j => 16 div
     }
+
     //create row
     sketchContainer.appendChild(rowContainer);
+}
+
+function setColor() {
+    // console.log(this);
+    this.style.backgroundColor = "black";
 }
