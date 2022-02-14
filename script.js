2cc15627ba42f4ac9a2609b57e9a2176ad307bdc
kgb <kgiandepot@gmail.com>
2022-02-14 15:34:49 +0200

    Fix sketchContainer dimenesions

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 const sketchContainer = document.querySelector('#sketchContainer');
+
 let gridSize = 16; // 16 x 16 grid;
 
 //game
 for (let i = 0; i < gridSize; i++) {    //i = row
+    const rowContainer = getDomRow();
+    for (let j = 0; j < gridSize; j++) {   //j = column
+        const div = document.createElement('div');
+        setRowSquare(div);
+        rowContainer.appendChild(div);
+    }
+    sketchContainer.appendChild(rowContainer);
+
+}
+
+function setRowSquare(div) {
+    div.className = 'gridDiv';
+    div.addEventListener('mouseover', setColor);
+    div.style.backgroundColor = 'blue';
+
+    // console.log(sketchContainer.clientWidth); 
+
+    div.style.width = `${sketchContainer.clientWidth / gridSize}px`;
+    div.style.height = `${sketchContainer.clientHeight / gridSize}px`;
+}
+
+function getDomRow() {
     const rowContainer = document.createElement('div');
     rowContainer.className = 'rowContainer';
     rowContainer.setAttribute('style', 'display: flex;');
-    
-    for (let j = 0; j < gridSize; j++) {   //j = column
-        const div = document.createElement('div');
-        div.className = 'gridDiv';
-        div.addEventListener('mouseover', setColor);
-        div.textContent = `${i},${j}`;
-        div.style.backgroundColor = 'blue';
-        
-        div.setAttribute('style', 'width: 20px; height: 20px;');
-        
-        rowContainer.appendChild(div);
-        //create div for each j => 16 div
-    }
-    
-    //create row
-    sketchContainer.appendChild(rowContainer);
+    return rowContainer;
 }
 
 function setColor() {
     // console.log(this);
-    this.style.backgroundColor = "black";
+    this.style.backgroundColor = "lightblue";
 }
 function setYear() {
     document.querySelector(".date").textContent = new Date().getFullYear();
 }
 setYear();
+
