e6ed9cb04999f9b2c8776febd72e7870d112b653
kgb <kgiandepot@gmail.com>
2022-02-15 09:18:56 +0200

    Add setGrid function

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const sketchContainer = document.querySelector('#sketchContainer');
+const eraser = document.querySelector('#eraser');
+const clear = document.querySelector('#clear');
+const newSketch = document.querySelector('#newSketch');
+const psyStylus = document.querySelector('#psyStylus');
 
-let gridSize = getGridSize();
+let gridSize = 16;
+// let gridSize = getGridSize();
+setGrid(gridSize);
 
-
-//game
-for (let i = 0; i < gridSize; i++) {    //i = row
-    const rowContainer = getDomRow();
-    for (let j = 0; j < gridSize; j++) {   //j = column
-        const div = document.createElement('div');
-        setRowSquare(div);
-        rowContainer.appendChild(div);
+//sketchPad
+function setGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {    //i = row
+        const rowContainer = getDomRow();
+        for (let j = 0; j < gridSize; j++) {   //j = column
+            const div = document.createElement('div');
+            setRowSquare(div);
+            rowContainer.appendChild(div);
+        }
+        sketchContainer.appendChild(rowContainer);
     }
-    sketchContainer.appendChild(rowContainer);
-
 }
+
+
+
+
 
 function setRowSquare(div) {
     div.className = 'gridDiv';
@@ -22,7 +32,6 @@ function setRowSquare(div) {
     div.style.width = `${sketchContainer.clientWidth / gridSize}px`;
     div.style.height = `${sketchContainer.clientHeight / gridSize}px`;
 }
-
 function getGridSize() {
     let gridSize = 16;
     do {
@@ -30,14 +39,12 @@ function getGridSize() {
     } while (isNaN(gridSize) || gridSize > 100 || gridSize == '')
     return gridSize;
 }
-
 function getDomRow() {
     const rowContainer = document.createElement('div');
     rowContainer.className = 'rowContainer';
     rowContainer.setAttribute('style', 'display: flex;');
     return rowContainer;
 }
-
 function setColor() {
     this.style.backgroundColor = "lightblue";
 }
